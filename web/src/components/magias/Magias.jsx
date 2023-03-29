c9f537052e83c9beb9e2c03c5a853dffd7c41d8b
ubiratanlsc <ubiratanlsc@gmail.com>
2023-03-29 19:00:04 -0300

    magias maps

--- web/src/components/magias/Magias.jsx
+++ web/src/components/magias/Magias.jsx
@@ -4,13 +4,11 @@ import '../../assets/css/style.css'
 import api from "../../services/api";
 
 export const Magias = () => {
-    
-    const [value, setValue] = useState([])
     const [magic, setMagic] = useState([])
     const handleActive = (btn) => setIsActive(btn)
     const handleActive2 = (btn2) => setIsActive2(btn2)
     const handleActive3 = (btn3) => setIsActive3(btn3)
-    // const handleActive4 = (btn4) => setIsActive3(btn4)
+    const handleActive4 = (btn4) => setIsActive4(btn4)
     const [isActive, setIsActive] = useState(1)
     const [isActive2, setIsActive2] = useState(1)
     const [isActive3, setIsActive3] = useState()
@@ -18,11 +16,15 @@ export const Magias = () => {
     const [isActive5, setIsActive5] = useState()
 
     useEffect(() => {
-        api.get("magias").then(({data}) => {
+        api.get("magias").then(({ data }) => {
             setMagic(data)
         })
-        console.log(magic);        
+        // console.log(magic[0]._id);        
     }, [])
+    // console.log(isActive3); 9,24,32,14,52,2
+    const arcanas = magic.filter(tipo => (tipo.tipo == "arcana"))
+    const divinas = magic.filter(tipo => (tipo.tipo == "divina"))
+
     const niveis = [
         { id: 0, title: "nivel", sigla: "n0" },
         { id: 1, title: "nivel", sigla: "n1" },
@@ -34,10 +36,11 @@ export const Magias = () => {
         { id: 7, title: "nivel", sigla: "n7" },
         { id: 8, title: "nivel", sigla: "n8" },
         { id: 9, title: "nivel", sigla: "n9" },
-    ]   
+    ]
+
     return (
         <div className='tabsContainer' >
-            
+
             <h1>Magias</h1>
             <div className="btnContainer">
                 <button className={`tabs ${isActive === 1 ? 'activeTab' : ''}`}
@@ -47,29 +50,29 @@ export const Magias = () => {
                 <button className={`tabs ${isActive === 3 ? 'activeTab' : ''}`}
                     onClick={() => handleActive(3)}>Jutsus</button>
             </div>
-           
+
             {
                 isActive === 1 && <div className="tabData">
                     <div className="tabContent">
                         <div className="btnContainer">
                             {niveis.map((index, indice) =>
                                 <button className={`tabs ${isActive2 === index.id ? 'activeTab' : ''}`} key={indice}
-                                    onClick={() => handleActive2(index.id, handleActive3(magic[`${index.sigla}`]))}>{index.title}-{index.id}</button>
+                                    onClick={() => handleActive2(index.id)}>{index.title}-{index.id}</button>
                             )}
                         </div>
                     </div>
                 </div>}
             {
                 isActive === 2 && <div className="tabData">
-                <div className="tabContent">
-                    <div className="btnContainer">
-                        {niveis.map((index, indice) =>
-                            <button className={`tabs ${isActive4 === index.id ? 'activeTab' : ''}`} key={indice}
-                                onClick={() => handleActive2(index.id, handleActive5(magic[`${index.sigla}`]))}>{index.title}-{index.id}</button>
-                        )}
+                    <div className="tabContent">
+                        <div className="btnContainer">
+                            {niveis.map((index, indice) =>
+                                <button className={`tabs ${isActive4 === index.id ? 'activeTab' : ''}`} key={indice}
+                                    onClick={() => handleActive4(index.id, console.log(index.id))}>{index.title}-{index.id}</button>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
             }
             {
                 isActive === 3 && <div className="tabData">
@@ -82,24 +85,21 @@ export const Magias = () => {
                 </div>
             }
             {
-                niveis.map((ind, indexx) => isActive2 === ind.id && <div className="tabData" key={indexx} >
+                niveis.map((ind, indexx) =>isActive === 1 && isActive2 === ind.id && <div className="tabData" key={indexx} >
                     <div className="tabContent" key={ind.title}>
-                        {isActive3?.map((index, indce) => <h5 key={indce}>{index.name}</h5>)}
-
-
-                        <p>Updated: February 19, 2023</p>
+                        {arcanas?.map((index, indce) => isActive2 === index.nivel &&
+                        <h5 key={indce}>{index.titulo}</h5>)}
+                        <p>Updated: February 19, 2023</p> 
                     </div>
                 </div>)
-
-
             }
             {
-                niveis.map((ind, indexx) => isActive4 === ind.id && <div className="tabData" key={indexx} >
+                niveis.map((ind, indexx) => isActive === 2 && isActive4 === ind.id && <div className="tabData" key={indexx} >
                     <div className="tabContent" key={ind.title}>
-                        {isActive5?.map((index, indce) => <h5 key={indce}>{index.name}</h5>)}
-
-
+                        {divinas?.map((index, indce) => isActive4 === index.nivel && 
+                        <h5 key={indce}>{index.titulo}</h5>)}
                         <p>Updated: February 19, 2023</p>
+                        <p>teste divino</p>
                     </div>
                 </div>)
 
