dfcc97cd6b88e256fc7e340490f39c456b313a31
Ubiratan Lima <69194011+ubiratanlsc@users.noreply.github.com>
2023-11-21 21:42:51 -0300

    grimorio v0.0.2

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,13 @@
 import Sidebar from './sidebar/Sidebar';
-import '../src/assets/css/style.css'
-import { Outlet } from 'react-router-dom';
+import Grimorio from './components/grimorio/Grimorio';
+import './index.css'
 function App() {
-
- 
   return (
-    <>
+    <div className="bg-hero bg-cover">
       <Sidebar>
-        <Outlet/>
+        <Grimorio></Grimorio>
       </Sidebar>
-    </>
+    </div>
   )
 }
 
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,44 +1,10 @@
-import React, { Children } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App'
-import { Magias } from './components/magias/Magias';
-import { Personagens } from './components/personagens/Personagens'
+// import './index.css'
 
-
-
-const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <Personagens />
-  // },
-  // {
-  //   path: "/magias",
-  //   element: <Magias />
-  // },
-  // {
-  //   path: "/app",
-  //   element: <App />
-  // }
-
-  {
-    path: "/",
-    element:<App />,
-    // errorElement: <Error />
-    children: [
-       {
-          path: "/",
-          element: <Personagens />
-        },
-        {
-          path: "/magias",
-          element: <Magias />
-        },
-    ]
-  }
-])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
